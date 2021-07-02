0ba33e41b1fd8982b5a8800f39bf54ad824ba036
Ameema Zainab <ameemazainab@gmail.com>
2021-07-02 23:50:09 +0300

    Add files via upload

---
+++ ShortCourseexamples500.py
@@ -0,0 +1,29 @@
+import time
+import signal
+import numpy as np
+import os
+import csv
+import pandas as pd
+import itertools as it
+
+import multiprocessing as ms
+import threading
+from multiprocessing.pool import ThreadPool
+from multiprocessing import Pool
+from datetime import datetime
+from functools import partial
+
+df = pd.read_csv('/lustre/home/amzaina65/ShortCourse/sampledata500.csv')
+count = df.count()
+print("Count of data records", count)
+
+count_meters = df['meter'].unique()
+print("Count of unique meters", count_meters)
+
+time1 = time.time()
+for i in count_meters:
+    meter_df = df[df.meter == i]
+    print("count of meter " % i, meter_df.count())
+print("total time taken", time.time() - time1)
+  
+  
