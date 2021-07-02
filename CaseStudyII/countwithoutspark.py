25fe3addadd5da9324518c683fff99f546b84d19
Ameema Zainab <ameemazainab@gmail.com>
2021-07-03 00:54:00 +0300

    Update countwithoutspark.py

--- CaseStudyII/countwithoutspark.py
+++ CaseStudyII/countwithoutspark.py
@@ -1,17 +1,8 @@
 import time
 import signal
 import numpy as np
-import os
 import csv
 import pandas as pd
-import itertools as it
-
-import multiprocessing as ms
-import threading
-from multiprocessing.pool import ThreadPool
-from multiprocessing import Pool
-from datetime import datetime
-from functools import partial
 
 df = pd.read_csv('/lustre/home/amzaina65/ShortCourse/sampledata500M.csv')
 time1 = time.time()
